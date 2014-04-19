b797c1139da0a882cb54eb3f51f856a8c0149bec
Adrien Ott <adrien.ott@gmail.com>
2014-04-19 18:40:09 +0200

    All files: First commit. Downlad last french airspace to "prev" folder

---
+++ get_fr_airspace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mv -f *AIRSPACE*.txt prev/.
+
+rm /tmp/tmp1.txt /tmp/tmp2.txt
+
+python get_url.py > /tmp/tmp1.txt
+
+awk 'NR==1' /tmp/tmp1.txt > /tmp/tmp2.txt
+
+cd prev
+wget -i /tmp/tmp2.txt
+
+echo " "
+echo "Most recent file is:"
+ls -tr | tail -1
+
+cd ..
